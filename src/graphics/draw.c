b831f50bb325ee5340d700b22f68f19a4b70757d
Aydin Ramezani <aydinramzni@gmail.com>
2025-11-22 12:27:32 +0330

    Changed some var names

--- src/graphics/draw.c
+++ src/graphics/draw.c
@@ -4,38 +4,38 @@
 #include <stdlib.h>
 
 void
-draw_pixel (Framebuffer *fb, Pixel_t pixel)
+draw_pixel (Framebuffer *fb, Pixel_t p)
 {
-  set_pixel (fb, pixel.pos, pixel.color);
+  set_pixel (fb, p.pos, p.color);
 }
 
 void
-draw_line (Framebuffer *fb, Pixel_t start, Pixel_t end)
+draw_line (Framebuffer *fb, Pixel_t p1, Pixel_t p2)
 {
   /* starting and ending coordinates */
-  int x0 = start.pos.x;
-  int y0 = start.pos.y;
-  int x1 = end.pos.x;
-  int y1 = end.pos.y;
+  int x1 = p1.pos.x;
+  int y1 = p1.pos.y;
+  int x2 = p2.pos.x;
+  int y2 = p2.pos.y;
 
   /* variables used by bresenham's algorithm */
   int dx, dy, sx, sy, err;
 
   /* initialize bresenham algorithm state */
-  bresenham_init (x0, y0, x1, y1, &dx, &dy, &sx, &sy, &err);
+  bresenham_init (x1, y1, x2, y2, &dx, &dy, &sx, &sy, &err);
 
   /* euclidean distance between start and end points */
-  float length = vec2i_distance (start.pos, end.pos);
+  float length = vec2i_distance (p1.pos, p2.pos);
 
   /* special case where start and end are the same pixel */
   if (length == 0.0f)
     {
-      draw_pixel (fb, start);
+      draw_pixel (fb, p1);
       return;
     }
 
   /* number of steps which is the length of the dominant axis */
-  int steps = abs (x1 - x0) > abs (y1 - y0) ? abs (x1 - x0) : abs (y1 - y0);
+  int steps = abs (x2 - x1) > abs (y2 - y1) ? abs (x2 - x1) : abs (y2 - y1);
 
   /* interpolation increment per pixel step */
   float dt = steps > 0 ? 1.0f / steps : 1.0f;
@@ -47,20 +47,20 @@ draw_line (Framebuffer *fb, Pixel_t start, Pixel_t end)
   for (;;)
     {
       Pixel_t p;
-      p.pos.x = x0;
-      p.pos.y = y0;
+      p.pos.x = x1;
+      p.pos.y = y1;
 
       /* interpolate each color channel base on the current t */
-      p.color.r = math_lerpi (start.color.r, end.color.r, t);
-      p.color.g = math_lerpi (start.color.g, end.color.g, t);
-      p.color.b = math_lerpi (start.color.b, end.color.b, t);
-      p.color.a = math_lerpi (start.color.a, end.color.a, t);
+      p.color.r = math_lerpi (p1.color.r, p2.color.r, t);
+      p.color.g = math_lerpi (p1.color.g, p2.color.g, t);
+      p.color.b = math_lerpi (p1.color.b, p2.color.b, t);
+      p.color.a = math_lerpi (p1.color.a, p2.color.a, t);
 
       /* draw the pixel */
       draw_pixel (fb, p);
 
       /* advance to the next pixel and break if it reached end */
-      if (!bresenham_step (&x0, &y0, x1, y1, &err, dx, dy, sx, sy))
+      if (!bresenham_step (&x1, &y1, x2, y2, &err, dx, dy, sx, sy))
         break;
 
       t += dt;
--- src/graphics/draw.h
+++ src/graphics/draw.h
@@ -5,9 +5,11 @@
 #include "platform/framebuffer.h"
 
 /* wrapper for set_pixel */
-void draw_pixel (Framebuffer *fb, Pixel_t pixel);
+void draw_pixel (Framebuffer *fb, Pixel_t p);
 
 /* guess what */
-void draw_line  (Framebuffer* fb, Pixel_t start, Pixel_t end);
+void draw_line  (Framebuffer* fb, Pixel_t p1, Pixel_t p2);
+
+void draw_triangle_fill(Framebuffer* fb, Pixel_t p1, Pixel_t p2, Pixel_t p3);
 
 #endif /* DRAW_H */
