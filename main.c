251fe7f8ec38668061be22f5b443cd92fa31b753
Aydin Ramezani <aydinramzni@gmail.com>
2025-11-27 09:33:17 +0330

    test: renders a rotation cube with depth testing

--- main.c
+++ main.c
@@ -23,16 +23,17 @@ typedef struct {
 typedef struct {
   Mat4x4f_t model;
   Mat4x4f_t view;
-  Mat4x4f_t proj;
+  Mat4x4f_t projection;
 } MeshTransforms;
 
 typedef struct {
-  VertexBuffer* vertexBuffer;
-  IndexBuffer*  indexBuffer;
+  VertexBuffer vertexBuffer;
+  VertexLayout vertexLayout;
+  IndexBuffer  indexBuffer;
 } MeshGeometry;
 
 typedef struct {
-  MeshGeometry*   geometry;
+  MeshGeometry    geometry;
   MeshTransforms  transforms;
 } MeshInstance;
 
@@ -72,8 +73,8 @@ void handle_input(Input* input, Camera* camera)
   // movement
   if (input->keys['w']) camera->eye = vec3f_sub(camera->eye, vec3f_mul_scalar(forward,  camera->move_speed));
   if (input->keys['s']) camera->eye = vec3f_add(camera->eye, vec3f_mul_scalar(forward,  camera->move_speed));
-  if (input->keys['d']) camera->eye = vec3f_add(camera->eye, vec3f_mul_scalar(right,    camera->move_speed));
-  if (input->keys['a']) camera->eye = vec3f_sub(camera->eye, vec3f_mul_scalar(right,    camera->move_speed));
+  if (input->keys['a']) camera->eye = vec3f_add(camera->eye, vec3f_mul_scalar(right,    camera->move_speed));
+  if (input->keys['d']) camera->eye = vec3f_sub(camera->eye, vec3f_mul_scalar(right,    camera->move_speed));
 
   // rotation
   if (input->keys['h']) camera->yaw -= camera->rotate_speed;
@@ -86,6 +87,44 @@ void handle_input(Input* input, Camera* camera)
   if (input->keys['j']) {
     camera->pitch -= camera->rotate_speed;
     if (camera->pitch < -PI / 2.0f + 0.01f) camera->pitch = -PI / 2.0f + 0.01f;
+  }
+}
+
+static inline Mat4x4f_t build_mvp(const MeshTransforms* t, const Camera* cam) {
+  Mat4x4f_t view        = mat4x4f_lookat(cam->eye, cam->target, cam->up);
+  Mat4x4f_t projection  = mat4x4f_perspective(cam->fov, cam->aspect, cam->near, cam->far);
+
+  Mat4x4f_t mvp = mat4x4f_mul(&view, &t->model);
+  mvp           = mat4x4f_mul(&projection, &mvp);
+
+  return mvp;
+}
+
+static inline void update_vertices(VertexBuffer* vb,
+                                   const Vertex* source,
+                                   const Mat4x4f_t* mvp)
+{
+  for (size_t i = 0; i < vb->vertex_count; ++i) {
+    Vec3f_t* pos = get_attribute_pointer(vb, i, ATTR_POSITION);
+
+    Vec4f_t v = {
+      source[i].pos.x,
+      source[i].pos.y,
+      source[i].pos.z,
+      1.0f
+    };
+
+    Vec4f_t t = mat4x4f_mul_vec4f(mvp, &v);
+
+    if (t.w != 0.0f) {
+      t.x /= t.w;
+      t.y /= t.w;
+      t.z /= t.w;
+    }
+
+    pos->x = t.x;
+    pos->y = t.y;
+    pos->z = t.z;
   }
 }
 
@@ -112,19 +151,79 @@ int main (void)
     0, 1, 2
   };
 
+  const Vertex cube_vertices[] = {
+    // front face
+    { .pos = { -0.5f, -0.5f,  0.5f }, .col = { 1.0f, 0.0f, 0.0f, 1.0f } }, // 0
+    { .pos = {  0.5f, -0.5f,  0.5f }, .col = { 1.0f, 1.0f, 0.0f, 1.0f } }, // 1
+    { .pos = {  0.5f,  0.5f,  0.5f }, .col = { 0.0f, 1.0f, 0.0f, 1.0f } }, // 2
+    { .pos = { -0.5f,  0.5f,  0.5f }, .col = { 0.0f, 1.0f, 1.0f, 1.0f } }, // 3
+
+    // back face
+    { .pos = { -0.5f, -0.5f, -0.5f }, .col = { 1.0f, 0.0f, 1.0f, 1.0f } }, // 4
+    { .pos = {  0.5f, -0.5f, -0.5f }, .col = { 1.0f, 0.5f, 0.0f, 1.0f } }, // 5
+    { .pos = {  0.5f,  0.5f, -0.5f }, .col = { 0.0f, 0.5f, 1.0f, 1.0f } }, // 6
+    { .pos = { -0.5f,  0.5f, -0.5f }, .col = { 0.5f, 0.0f, 1.0f, 1.0f } }  // 7
+  };
+
+  const unsigned int cube_indices[] = {
+    // front
+    0, 1, 2,
+    2, 3, 0,
+
+    // right
+    1, 5, 6,
+    6, 2, 1,
+
+    // back
+    5, 4, 7,
+    7, 6, 5,
+
+    // left
+    4, 0, 3,
+    3, 7, 4,
+
+    // top
+    3, 2, 6,
+    6, 7, 3,
+
+    // bottom
+    4, 5, 1,
+    1, 0, 4
+  };
+
   // define attributes
   VertexAttribute attributes[] = {
     { ATTR_POSITION, offsetof(Vertex, pos), sizeof(float), 3 },
     { ATTR_COLOR,    offsetof(Vertex, col), sizeof(float), 4 }
   };
-  VertexLayout layout       = vertex_layout_create(attributes, COUNT_OF_ARRAY(attributes), sizeof(Vertex));
 
-  // create triangle buffers
-  VertexBuffer triangle_vb  = vertex_buffer_create(triangle_vertices, layout, COUNT_OF_ARRAY(triangle_vertices));
-  IndexBuffer  triangle_ib  = index_buffer_create(triangle_indices, COUNT_OF_ARRAY(triangle_indices));
+  // define triangle
+  MeshInstance triangle;
+
+  // initialize triangle geometry
+  triangle.geometry.vertexLayout = vertex_layout_create(attributes, COUNT_OF_ARRAY(attributes), sizeof(Vertex));
+  triangle.geometry.vertexBuffer = vertex_buffer_create(triangle_vertices, triangle.geometry.vertexLayout, COUNT_OF_ARRAY(triangle_vertices));
+  triangle.geometry.indexBuffer  = index_buffer_create(triangle_indices, COUNT_OF_ARRAY(triangle_indices));
+
+  // initialize triangle transforms
+  triangle.transforms.model       = mat4x4f_zero();
+  triangle.transforms.view        = mat4x4f_zero();
+  triangle.transforms.projection  = mat4x4f_zero();
+
+  // define cube
+  MeshInstance cube;
+
+  // initialize cube geometry
+  cube.geometry.vertexLayout = vertex_layout_create(attributes, COUNT_OF_ARRAY(attributes), sizeof(Vertex));
+  cube.geometry.vertexBuffer = vertex_buffer_create(cube_vertices, cube.geometry.vertexLayout, COUNT_OF_ARRAY(cube_vertices));
+  cube.geometry.indexBuffer  = index_buffer_create(cube_indices, COUNT_OF_ARRAY(cube_indices));
+
+  // initialize cube transforms
+  cube.transforms.model       = mat4x4f_zero();
+  cube.transforms.view        = mat4x4f_zero();
+  cube.transforms.projection  = mat4x4f_zero();
 
   float angle = 0.0f;
-  MeshTransforms transformMatrices;
 
   // initialize camera
   Camera camera = {
@@ -151,51 +250,29 @@ int main (void)
     input_update(&input);
     handle_input(&input, &camera);
 
-    // model matrix
-    Vec3f_t rotation = { 0.0f, 0.0f, 0.0f };
-    transformMatrices.model = mat4x4f_identity();
-    transformMatrices.model = mat4x4f_rotation(&transformMatrices.model, rotation);
+    // update triangle transoforms
+    // triangle.transforms.model = mat4x4f_identity();
+    // triangle.transforms.model = mat4x4f_rotation(&triangle.transforms.model, (Vec3f_t){angle, angle, angle});
+    // update cube transforms
+    cube.transforms.model = mat4x4f_identity();
+    cube.transforms.model = mat4x4f_rotation(&cube.transforms.model, (Vec3f_t){angle, angle, angle});
 
-    // view matrix
-    transformMatrices.view    = mat4x4f_lookat(camera.eye, camera.target, camera.up);
-
-    // projection matrix
-    transformMatrices.proj    = mat4x4f_perspective(camera.fov, camera.aspect, camera.near, camera.far);
-
-    // combine to MVP matrix
-    Mat4x4f_t mvp = mat4x4f_mul(&transformMatrices.view, &transformMatrices.model);
-    mvp           = mat4x4f_mul(&transformMatrices.proj, &mvp);
+    // create triangle mvp
+    // Mat4x4f_t triangle_mvp = build_mvp(&triangle.transforms, &camera);
+    // create cube mvp
+    Mat4x4f_t cube_mvp = build_mvp(&cube.transforms, &camera);
 
     // update triangle vertices
-    for (size_t i = 0; i < triangle_vb.vertex_count; ++i) {
-      // get the position attribute for the vertex
-      Vec3f_t* position = get_attribute_pointer(&triangle_vb, i, ATTR_POSITION);
-
-      // convert to homogeneous coordinates
-      Vec4f_t vertex_position = {
-        triangle_vertices[i].pos.x,
-        triangle_vertices[i].pos.y,
-        triangle_vertices[i].pos.z,
-        1.0f };
-
-      // apply the MVP matrix
-      Vec4f_t transformed_vertex = mat4x4f_mul_vec4f(&mvp, &vertex_position);
-
-      // apply perspective division
-      if (transformed_vertex.w != 0.0f) {
-        transformed_vertex.x /= transformed_vertex.w;
-        transformed_vertex.y /= transformed_vertex.w;
-        transformed_vertex.z /= transformed_vertex.w;
-      }
-
-      // update triangle vertex position
-      position->x = transformed_vertex.x;
-      position->y = transformed_vertex.y;
-      position->z = transformed_vertex.z;
-    }
-
+    // update_vertices(&triangle.geometry.vertexBuffer,
+    //                 triangle_vertices,
+    //                 &triangle_mvp);
+    // update cube vertices
+    update_vertices(&cube.geometry.vertexBuffer,
+                    cube_vertices,
+                    &cube_mvp);
     fb_clear(&fb);
-    draw_index_buffer(&fb, &triangle_ib, &triangle_vb, PRIM_TRIANGLES);
+    // draw_index_buffer(&fb, &triangle.geometry.indexBuffer, &triangle.geometry.vertexBuffer, PRIM_TRIANGLES);
+    draw_index_buffer(&fb, &cube.geometry.indexBuffer, &cube.geometry.vertexBuffer, PRIM_TRIANGLES);
     fb_present(&fb);
 
     // update angle
@@ -208,8 +285,8 @@ int main (void)
   }
 
   // cleanup and exit
-  vertex_buffer_destroy(&triangle_vb);
-  index_buffer_destroy(&triangle_ib);
+  vertex_buffer_destroy(&triangle.geometry.vertexBuffer);
+  index_buffer_destroy(&triangle.geometry.indexBuffer);
   input_shutdown();
   fb_shutdown(&fb);
 
